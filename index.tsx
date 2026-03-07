d7e775a0043b456971f4fa944cb3d1c16129da5d
DAVEVERA <info@davevera.nl>
2026-03-07 22:31:00 +0100

    fix: resolve runtime router crash and add favicon

--- index.tsx
+++ index.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import { LanguageProvider } from './contexts/LanguageContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { LanguageProvider } from "./contexts/LanguageContext";
 
-const rootElement = document.getElementById('root');
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+
+const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
@@ -12,8 +15,12 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <LanguageProvider>
-      <App />
-    </LanguageProvider>
-  </React.StrictMode>
+    <HelmetProvider>
+      <BrowserRouter>
+        <LanguageProvider>
+          <App />
+        </LanguageProvider>
+      </BrowserRouter>
+    </HelmetProvider>
+  </React.StrictMode>,
 );
